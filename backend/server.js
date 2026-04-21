e3591a6a37e0a9ee16ecc9f3c0c76e3015087a23
Lalu27 <firmansyahmuhasan@gmail.com>
2026-04-21 16:52:14 +0700

    Update server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => res.send('API Sewa Lapangan Running...'));
 
